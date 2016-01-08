ae55dec0cac41d4d70dea9f740664090e852310a
RYNE.WANG <Ryne.Wang@cootek.cn>
2016-01-08 20:46:39 +0800

    flappy

---
+++ Assets/_Script/MiniGame/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	// The force which is added when the player jumps
+	// This can be changed in the Inspector window
+	public Vector2 jumpForce = new Vector2(0, 300);
+	public Vector2 jumpVelocity = new Vector2(0,5);
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Jump
+		if (Input.GetKeyUp("space"))
+		{
+//			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+//			GetComponent<Rigidbody2D>().AddForce(jumpForce);
+			GetComponent<Rigidbody2D>().velocity = jumpVelocity;
+		}
+		
+		// Die by being off screen
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0)
+		{
+			Die();
+		}
+	}
+	
+	// Die by collision
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Die();
+	}
+	
+	void Die()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
